0dba253e40fac5c295f4d23df21ef307f6fc422b
Deathwong <baptistejean457@gmail.com>
2023-05-30 09:29:17 +0200

    init commit

---
+++ src/main/java/com/jeff/electroniquesign/ElectroniqueSignApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.electroniquesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroniqueSignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectroniqueSignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeff/electroniquesign/ElectroniqueSignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.electroniquesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroniqueSignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
