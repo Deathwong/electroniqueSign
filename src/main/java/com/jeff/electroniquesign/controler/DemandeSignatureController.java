8a0ea2f937867a723688fadf69d11a4dfcdfa7bc
Deathwong <baptistejean457@gmail.com>
2023-06-02 19:59:09 +0200

    test controller demande signature

--- src/main/java/com/jeff/electroniquesign/controler/DemandeSignatureController.java
+++ src/main/java/com/jeff/electroniquesign/controler/DemandeSignatureController.java
@@ -1,18 +1,15 @@
 package com.jeff.electroniquesign.controler;
 
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/electroniquesign")
-@AllArgsConstructor
-public class ElectroniqueSignController {
-
+public class DemandeSignatureController {
 
     @GetMapping("/demandesignature")
     String consulterDemandeSignature() {
-        return "string";
+        return "Hello Stephan";
     }
 }
